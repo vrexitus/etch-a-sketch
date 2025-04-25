059a724fa8472fb85423a0706b08b5b0126d24dc
vrexitus <98414540+vrexitus@users.noreply.github.com>
2025-04-25 13:29:41 +0100

    completed the exercise

---
+++ script.js
@@ -0,0 +1,44 @@
+const container = document.querySelector(".container");
+
+let buttonContainer = document.createElement("div");
+buttonContainer.classList.add("buttonContainer");
+let button = document.createElement("button");
+button.textContent = "GENERATE";
+button.classList.add("resetButton");
+buttonContainer.appendChild(button);
+button.addEventListener("click", resetGrid);
+container.appendChild(buttonContainer);
+
+
+function generateGrid(numOfSquares) {
+    let gridContainer = document.createElement("div");
+    gridContainer.classList.add("gridContainer");
+    container.appendChild(gridContainer);
+    for (let i = 1; i <= numOfSquares*numOfSquares; i++) {
+        let gridItem = document.createElement("div");
+        let color = random_rgba();
+        gridItem.textContent = i;
+        gridItem.classList.add("gridItem");
+        gridItem.style.flexBasis = "calc(100%/" + String(numOfSquares).toString() + ")";
+        gridItem.addEventListener("mouseover", () => {
+            gridItem.style.backgroundColor = color;
+        });
+        gridContainer.appendChild(gridItem);
+    }
+    return (gridContainer);
+}
+let gridValue = generateGrid(16);
+
+function random_rgba() {
+    var o = Math.round, r = Math.random, s = 255;
+    return 'rgba(' + o(r()*s) + ',' + o(r()*s) + ',' + o(r()*s) + ',' + r().toFixed(1) + ')';
+}
+
+function resetGrid(){
+    gridValue.remove();
+    let newSquares = 0;
+    do {
+        newSquares = parseInt(prompt("How many squares?:  (Enter a number between 0 and 100)"));
+    } while (newSquares > 100)
+    gridValue = generateGrid(newSquares);
+}
